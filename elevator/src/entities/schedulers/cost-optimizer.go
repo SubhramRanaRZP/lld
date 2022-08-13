7eaf7201a3f1c23be46a25caa41664117194f012
SubhramRanaRZP <subhram.rana@razorpay.com>
2022-08-13 13:01:05 +0530

    added class diagram

--- elevator/src/entities/schedulers/cost-optimizer.go
+++ elevator/src/entities/schedulers/cost-optimizer.go
@@ -9,6 +9,8 @@ type CostOptimizerScheduler struct {
 	name string
 }
 
+// Schedule contains the logic via which it will decide which elevator-car need to be stopped for the
+// move request
 func (s *CostOptimizerScheduler) Schedule(r *entities.MoveRequest) *entities.Car {
 	fmt.Printf("request: %+v has been scheduled according to %v",
 		*r, s.name)
